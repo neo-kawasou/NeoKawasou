e534537561554674eb3c556dfb8d2231d1e339fb
ema <neokawasou@gmail.com>
2026-01-06 20:09:11 +0900

    wip

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ export function Home() {
   return (
     <div>
       <div>home</div>
-      <p>ホーム画面です!!</p>
+      <p>ホーム画面です!!!!!!</p>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/NeoKawasou/',
-  plugins: [react()],       // ← これで TSX と Fast Refresh に対応
-  build: {
-    outDir: 'docs',
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
   },
 })
